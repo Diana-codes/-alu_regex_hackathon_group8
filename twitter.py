7263793772f0dbcd2fdcddbfc9177ef19b30acd5
keza <j.keza1@alustudent.com>
2024-02-11 12:52:25 -0800

    complete

--- twitter.py
+++ twitter.py
@@ -18,3 +18,23 @@ Date: 25-Dec-2022
 Color: #00FF00
 IP: 127.0.0.1
 
+Movie: The Godfather (1972)
+Song: [Verse 3] I'm a Barbie girl, in a Barbie world
+Twitter: @user456
+ISBN: ISBN 789-0-123-45678-9
+Joke: Why did the scarecrow win an award? Because he was outstanding in his field.
+Episode: Game of Thrones S05E03: High Sparrow
+Date: 15-Nov-2021
+Color: #0000FF
+IP: 192.168.0.1
+
+Movie: Pulp Fiction (1994)
+Song: [Verse 1] Sweet Caroline, ba ba ba
+Twitter: @jsmith789
+ISBN: ISBN 012-3-456-78901-2
+Joke: Why did the math book sad? Because it had too many problems.
+Episode: The Office S02E03: Office Olympics
+Date: 03-Oct-2020
+Color: #FF0000
+IP: 10.0.0.1
+"""
